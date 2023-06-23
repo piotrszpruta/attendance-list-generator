d4f5ce54a8810449f387500438a3a36012c84f32
Piotr Szpruta <piotrszpruta@yahoo.com>
2023-06-23 19:41:35 +0200

    Create Worker model

---
+++ Models/Worker.cs
@@ -0,0 +1,11 @@
+﻿namespace Attendance_List_Generator.Models;
+
+internal class Worker
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public long Pesel { get; set; }
+    public Workplace Workplace { get; set; }
+    public Agreement AgreementType { get; set; }
+}
