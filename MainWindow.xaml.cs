e569afd4c3df164f66e98e4c4ac733ee25917b30
Piotr Szpruta <piotrszpruta@yahoo.com>
2023-07-06 18:40:10 +0200

    Add Handy controls and change MainWindow to HandyControl Window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace Attendance_List_Generator
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : HandyControl.Controls.Window
     {
         public MainWindow()
         {
