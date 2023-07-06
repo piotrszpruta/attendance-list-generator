1dca58b3b24750aee7e2e53669ae56c70de04c9b
Piotr Szpruta <piotrszpruta@yahoo.com>
2023-07-06 18:41:56 +0200

    Create Startup Class to create path for database and required collections

---
+++ Helpers/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Attendance_List_Generator.Models;
+using LiteDB;
+using Microsoft.VisualBasic;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D.Converters;
+
+namespace Attendance_List_Generator.Helpers
+{
+    internal class Startup
+    {
+        private static readonly string _appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        private static readonly string _applicationPath = Path.Combine(_appDataFolder, "ALG_Data");
+
+        public static Task StartupManager()
+        {
+            if (IsDirectoryExisting())
+            {
+                InitDb();
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private static bool IsDirectoryExisting()
+        {
+            if (Directory.Exists(_applicationPath))
+            {
+                return true;
+            }
+            else
+            {
+                Directory.CreateDirectory(_applicationPath);
+                return true;
+            }
+        }
+
+        public static Task InitDb()
+        {
+            // Create or initialize database
+            using var db = new LiteDatabase(Path.Combine(_applicationPath, "alg.db"));
+
+            var workplaceCol = db.GetCollection<Workplace>("workplace");
+            var agreementCol = db.GetCollection<Agreement>("agreement");
+            var workersCol = db.GetCollection<Worker>("workers");
+
+            db.Commit();
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Attendance_List_Generator.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace Attendance_List_Generator
         public MainWindow()
         {
             InitializeComponent();
+            Startup.StartupManager();
         }
     }
 }
